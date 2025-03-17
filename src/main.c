63b9a8f5a16f700699df30e1957bc1b0cf1c4e07
abdullahjaved06 <javedabdullah963@gmail.com>
2025-03-17 22:52:48 +0500

    added device DT Node

--- src/main.c
+++ src/main.c
@@ -6,10 +6,16 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/i2c.h>
 
+static const struct i2c_dt_spec spl01_dev = I2C_DT_SPEC_GET(DT_NODELABEL(spl06));
 
 int main(void)
 {
-	
+	if(!device_is_ready(spl01_dev.bus))
+	{
+		printk("I2C bus not ready\n");
+        return;
+	}
 }
